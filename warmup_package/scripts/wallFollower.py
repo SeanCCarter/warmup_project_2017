49f88bf76d863c57be193189cde3b5013c5b7776
krusellp <paul.krusell@students.olin.edu>
2017-02-03 11:37:17 -0500

    adding working wall follower

--- warmup_package/scripts/wallFollower.py
+++ warmup_package/scripts/wallFollower.py
@@ -19,18 +19,27 @@ class wallFollower(object):
 
 	def processLaser(self, L):
 		"takes a bunch of data from the laser scanner and uses it"
-		self.front_point = L.ranges[315]
-		self.back_point = L.ranges[225]
-		# self.front_point = [p for p in L.ranges[310:320] if p != 0.0]
-		# self.back_point = [p for p in L.ranges[265:275] if p != 0.0]
+		# self.front_point = L.ranges[315]
+		# self.back_point = L.ranges[225]
+		self.middle = L.ranges[270]
+		front_points = [p for p in L.ranges[310:320] if p != 0.0]
+		self.front_point = sum(front_points)/len(front_points)
+		back_points = [p for p in L.ranges[220:230] if p != 0.0]
+		self.back_point = sum(back_points)/len(back_points)
 
+	def doMath(self, distance):
+		""" it does math to figure out the right distance away from
+		wall"""
+		rotational_corr = -.1*(self.front_point-self.back_point)
+		distance_corr = -.2*(self.middle - distance)
+		return rotational_corr + distance_corr
 
 	def run(self, distance=1):
 		forwards = Twist()
 		forwards.linear.x = .1
 		stop_command = Twist()
 		while not rospy.is_shutdown():
-			forwards.angular.z = -.9*(self.front_point-self.back_point)
+			forwards.angular.z = self.doMath(distance)
 			self.publisher.publish(forwards)
 			self.r.sleep()
 
